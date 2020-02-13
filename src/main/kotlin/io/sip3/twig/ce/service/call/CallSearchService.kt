96e3bdf020569420cbe60f97cb09d1b96335b85f
agafox <agafox@sip3.io>
2020-02-13 08:07:41 +0200

    Added: `SearchController` skeleton

--- src/main/kotlin/io/sip3/twig/ce/service/call/CallSearchService.kt
+++ src/main/kotlin/io/sip3/twig/ce/service/call/CallSearchService.kt
@@ -37,7 +37,7 @@ class CallSearchService : SearchService {
 
     companion object {
 
-        val COMPARATOR = compareBy<Document>(
+        val CREATED_AT = compareBy<Document>(
                 { d -> d.getLong("created_at") },
                 { d -> d.getString("dst_addr") }
         )
@@ -56,62 +56,121 @@ class CallSearchService : SearchService {
     private lateinit var mongoClient: MongoClient
 
     override fun search(request: SearchRequest): Iterator<SearchResponse> {
+        // TODO...
         return Collections.emptyIterator<SearchResponse>()
     }
 
-    private fun findInIndexBySearchRequest(request: SearchRequest): Iterator<Document> {
+    private fun findInRtprIndexBySearchRequest(request: SearchRequest): Iterator<Document> {
+        // TODO...
         return Collections.emptyIterator<Document>()
     }
 
-    private fun findInIndexByDocument(document: Document): Iterator<Document> {
-        val createdAt = document.getLong("created_at")
-        val terminatedAt = document.getLong("terminated_at")
+    private fun findInSipIndexBySearchRequest(request: SearchRequest): Iterator<Document> {
+        // TODO...
+        return Collections.emptyIterator<Document>()
+    }
 
-        val filters = mutableListOf<Bson>().apply {
-            // Time filters
-            if (terminatedAt == null) {
-                // Session is still in progress
-                add(or(
-                        gt("created_at", createdAt - terminationTimeout),
-                        lt("created_at", createdAt + terminationTimeout)
-                ))
-            } else {
-                // Session is terminated
-                add(lt("created_at", terminatedAt))
-                add(gt("terminated_at", createdAt))
+    inner class SearchIterator(private val request: SearchRequest, private val documents: Iterator<Document>) : Iterator<SearchResponse> {
+
+        private val processed = mutableSetOf<Document>()
+        private var next: Set<Document>? = null
+
+        override fun hasNext(): Boolean {
+            if (next == null) {
+                while (documents.hasNext()) {
+                    val document = documents.next()
+
+                    // Check if document is in `processed`
+                    if (!processed.contains(document)) {
+                        // Create and aggregate `call`
+                        val call = TreeSet(CREATED_AT)
+                        aggregateCall(document, call)
+
+                        // Add all documents to `processed`
+                        processed.addAll(call)
+
+                        // Check `created_at` condition
+                        if (request.createdAt <= call.first().getLong("created_at")) {
+                            next = call
+                            break
+                        }
+                    }
+                }
             }
 
-            // Host filters
-            add(or(
-                    document.getString("dst_host")?.let { eq("src_host", it) } ?: eq("dst_addr", document.getString("src_addr")),
-                    document.getString("src_host")?.let { eq("dst_host", it) } ?: eq("src_addr", document.getString("dst_addr"))
-            ))
-
-            // Main filters
-            val xCallId = document.getString("x_call_id")
-            add(or(
-                    and(
-                            eq("caller", document.getString("caller")),
-                            eq("callee", document.getString("callee"))
-                    ),
-                    if (xCallId == null) {
-                        eq("x_call_id", document.getString("call_id"))
-                    } else {
-                        or(
-                                eq("call_id", document.getString("x_call_id")),
-                                eq("x_call_id", document.getString("call_id")),
-                                eq("x_call_id", document.getString("x_call_id"))
-                        )
-                    }
-            ))
+            return next != null
         }
 
-        val timeRange = if (terminatedAt == null) {
-            Pair(createdAt - terminationTimeout, createdAt + terminationTimeout)
-        } else {
-            Pair(createdAt, terminatedAt)
+        override fun next(): SearchResponse {
+            // `NoSuchElementException` must be thrown accordingly to the `Iterator` class contract
+            if (!hasNext()) {
+                throw NoSuchElementException()
+            }
+
+            val result = SearchResponse().apply {
+                // TODO...
+            }
+
+            next = null
+            return result
         }
 
-        return mongoClient.find("sip_call_index", timeRange, and(filters))
+        private fun aggregateCall(document: Document, call: TreeSet<Document>) {
+            if (call.add(document) && call.size < maxLegs) {
+                findInSipIndexByDocument(document).forEach { d -> aggregateCall(d, call) }
+            }
+        }
+
+        private fun findInSipIndexByDocument(document: Document): Iterator<Document> {
+            val createdAt = document.getLong("created_at")
+            val terminatedAt = document.getLong("terminated_at")
+
+            val filters = mutableListOf<Bson>().apply {
+                // Time filters
+                if (terminatedAt == null) {
+                    // Session is still in progress
+                    add(or(
+                            gt("created_at", createdAt - terminationTimeout),
+                            lt("created_at", createdAt + terminationTimeout)
+                    ))
+                } else {
+                    // Session is terminated
+                    add(lt("created_at", terminatedAt))
+                    add(gt("terminated_at", createdAt))
+                }
+
+                // Host filters
+                add(or(
+                        document.getString("dst_host")?.let { eq("src_host", it) } ?: eq("dst_addr", document.getString("src_addr")),
+                        document.getString("src_host")?.let { eq("dst_host", it) } ?: eq("src_addr", document.getString("dst_addr"))
+                ))
+
+                // Main filters
+                val xCallId = document.getString("x_call_id")
+                add(or(
+                        and(
+                                eq("caller", document.getString("caller")),
+                                eq("callee", document.getString("callee"))
+                        ),
+                        if (xCallId == null) {
+                            eq("x_call_id", document.getString("call_id"))
+                        } else {
+                            or(
+                                    eq("call_id", document.getString("x_call_id")),
+                                    eq("x_call_id", document.getString("call_id")),
+                                    eq("x_call_id", document.getString("x_call_id"))
+                            )
+                        }
+                ))
+            }
+
+            val timeRange = if (terminatedAt == null) {
+                Pair(createdAt - terminationTimeout, createdAt + terminationTimeout)
+            } else {
+                Pair(createdAt, terminatedAt)
+            }
+
+            return mongoClient.find("sip_call_index", timeRange, and(filters))
+        }
     }
 }
